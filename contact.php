6d6795dc6ab05e5ed4594bb269e0a1902243a868
parkingApUy <parkingapuy@gmail.com>
2017-11-14 21:14:17 -0300

    Create contact.php

---
+++ contact.php
@@ -0,0 +1,27 @@
+<?php
+echo "HOLA";
+require_once "Mail.php";
+$from = "Web Master <webmaster@example.com>";
+$to = "Nobody <nobody@example.com>";
+$subject = "Test email using PHP SMTP with SSL\r\n\r\n";
+$body = "This is a test email message";
+$host = "smtp.zoho.com";
+$port = "465";
+$username = "info@parkingap.com.uy";
+$password = "fabrizio1717";
+$headers = array ('From' => $from,
+  'To' => $to,
+  'Subject' => $subject);
+$smtp = Mail::factory('smtp',
+  array ('host' => $host,
+    'port' => $port,
+    'auth' => true,
+    'username' => $username,
+    'password' => $password));
+$mail = $smtp->send($to, $headers, $body);
+if (PEAR::isError($mail)) {
+  echo("<p>" . $mail->getMessage() . "</p>");
+} else {
+  echo("<p>Message successfully sent!</p>");
+}
+?>
