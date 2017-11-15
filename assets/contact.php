bb1e2ecb97cc0d6b8f055dcc35d7e2b1ca1e9f22
parkingApUy <parkingapuy@gmail.com>
2017-11-14 21:03:47 -0300

    Update contact.php

--- assets/contact.php
+++ assets/contact.php
@@ -1,59 +1,31 @@
 <?php
+require_once "Mail.php";
 
+$from = "Web Master <webmaster@example.com>";
+$to = "Nobody <nobody@example.com>";
+$subject = "Test email using PHP SMTP with SSL\r\n\r\n";
+$body = "This is a test email message";
 
+$host = "smtp.zoho.com";
+$port = "465";
+$username = "info@parkingap.com.uy";
+$password = "fabrizio1717";
 
+$headers = array ('From' => $from,
+  'To' => $to,
+  'Subject' => $subject);
+$smtp = Mail::factory('smtp',
+  array ('host' => $host,
+    'port' => $port,
+    'auth' => true,
+    'username' => $username,
+    'password' => $password));
 
-    // Enter the email where you want to receive the message
-    $emailTo = 'info@parkingap.com.uy';
+$mail = $smtp->send($to, $headers, $body);
 
-    $clientEmail = addslashes(trim($_POST['email']));
-    $subject = addslashes(trim($_POST['subject']));
-    $message = addslashes(trim($_POST['message']));
-
-    $array = array('emailMessage' => '', 'subjectMessage' => '', 'messageMessage' => '');
-	
-	
-
-    if(!isEmail($clientEmail)) {
-        $array['emailMessage'] = 'Invalid email!';
-    }
-    if($subject == '') {
-        $array['subjectMessage'] = 'Empty subject!';
-    }
-    if($message == '') {
-        $array['messageMessage'] = 'Empty message!';
-    }
-    if(isEmail($clientEmail) && $subject != '' && $message != '') {
-        // Send email
-	
-
-	$headers = array(
-    'From' => $clientEmail,
-    'To' => $emailTo,
-    'Subject' => $subject
-	);
-		
-		
-		//$headers = "From: " . $clientEmail . " <" . $clientEmail . ">" . "\r\n" . "Reply-To: " . $clientEmail;
-		//mail($emailTo, $subject . " (lancar)", $message, $headers);
-		
-		$smtp = Mail::factory('smtp', array(
-        'host' => 'smtp.zoho.com',
-        'port' => '465',
-        'auth' => true,
-        'username' => 'info@parkingap.com.uy',
-        'password' => 'fabrizio1717'
-    ));
-
-$mail = $smtp->send($to, $headers, $message);
-
-
-
-    echo json_encode($array);
-
-
-
-
-
-
+if (PEAR::isError($mail)) {
+  echo("<p>" . $mail->getMessage() . "</p>");
+} else {
+  echo("<p>Message successfully sent!</p>");
+}
 ?>
